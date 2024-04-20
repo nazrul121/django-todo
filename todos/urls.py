19d085b11a072c410d327873f475dfcecdb68341
nazrul121 <info.n121@gmail.com>
2024-04-20 14:50:18 +0600

    CRUD is completed

--- todos/urls.py
+++ todos/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
+    path('markAsDone/<int:pk>/', views.markAsDone, name='mark-as-done'),
+    path('edit-task/<int:pk>/', views.editTask, name='edit-task'),
+    path('update-task/<int:pk>/', views.updateTask, name='update-task'),
+    path('delete-task/<int:pk>/', views.deleteTask, name='delete-task'),
+    
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
 from .models import Task
 # Create your views here.
 
@@ -8,6 +8,43 @@ def addTask(request):
         task = request.POST.get('task')
         # return HttpResponse(request)
         Task.objects.create(task=task)
-        return HttpResponse('Form has submitted')
+        return redirect('home')
 
     return HttpResponse('Only post method is allowed')
+
+
+
+def markAsDone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def editTask(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+
+    data = {
+        'item': task,
+    }
+
+    # return HttpResponse(data)  
+
+    return render(request, 'edit_task.html', data)
+
+
+def updateTask(request, pk):
+    if(request.method=='POST'):
+        task = get_object_or_404(Task, pk=pk)
+        # return HttpResponse(task)  
+        task.task = request.POST.get('task')
+        task.save()
+        return redirect('home')
+    else:
+        return redirect('home')
+
+
+def deleteTask(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
+    
